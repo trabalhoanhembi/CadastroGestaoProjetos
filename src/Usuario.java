c1d53a3cebb13975c4dd953cf7ab3095140273e3
trabalhoanhembi <trabalhoA3uam2025@gmail.com>
2025-09-17 16:28:38 -0300

    Update Usuario.java
    Atualização

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,28 +1,26 @@
-class Usuario {
-    // Atributos principais da clase Usuario
-    private final String nomeCompleto;    // Nome completo do usuário
-    private final String cpf;             // Número do CPF do usuário
-    private final String email;           // E-mail do usuário
-    private final String cargo;           // Cargo do usuário
-    private final String login;           // Login do usuário
-    private final String senha;           // Senha do usuário
-    private final String perfil;          // Perfil do projeto (aceitando os valores administrador, gerente ou colaborador)
+// Importa as classes necessárias
+import java.io.Serializable;
 
-    // Construtor da classe
+class Usuario extends Pessoa implements Serializable {
+    // Atributos principais da clase Usuario
+    private String cargo;   // Cargo do usuário
+    private String login;   // Login do usuário
+    private String senha;   // Senha do usuário
+    private String perfil;  // Perfil do projeto (aceitando os valores administrador, gerente ou colaborador)
+
+    // Construtor Padrão (Necessário para serialização)
+    public Usuario() { }
+
+    // Construtor principal para inicializar um usuário
     public Usuario(String nomeCompleto, String cpf, String email, String cargo, String login, String senha, String perfil) {
-        this.nomeCompleto = nomeCompleto;
-        this.cpf = cpf;
-        this.email = email;
+        super(nomeCompleto, cpf, email);
         this.cargo = cargo;
         this.login = login;
         this.senha = senha;
         this.perfil = perfil;
     }
 
-    //Getters, permitem acessar os atributos do usuario
-    public String getNomeCompleto() { return nomeCompleto; }
-    public String getCpf() { return cpf; }
-    public String getEmail() { return email; }
+    // Métodos getters para acessar os dados
     public String getCargo() { return cargo; }
     public String getLogin() { return login; }
     public String getSenha() { return senha; }
@@ -32,13 +30,12 @@ class Usuario {
     // É chamado automaticamente quando o objeto Usuario é impresso
     @Override
     public String toString() {
-        return "\n"
-                + "Nome completo: " + getNomeCompleto() + "\n"
+        return "Nome: " + getNome() + "\n"
                 + "CPF: " + getCpf() + "\n"
                 + "E-mail: " + getEmail() + "\n"
                 + "Cargo: " + getCargo() + "\n"
                 + "Login: " + getLogin() + "\n"
                 + "Senha: " + getSenha() + "\n"
-                + "Perfil: " + getPerfil() + "\n";
+                + "Perfil: " + getPerfil();
     }
 }
