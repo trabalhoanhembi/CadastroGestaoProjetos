ecb7b0c20c828feb5fd68e1f55748840779d637f
trabalhoanhembi <trabalhoA3uam2025@gmail.com>
2025-09-17 16:28:12 -0300

    Update Projeto.java
    Atualização

--- src/Projeto.java
+++ src/Projeto.java
@@ -1,13 +1,19 @@
-class Projeto {
-    // Atributos principais da clase Projeto
-    private final String nome;                // Nome do projeto
-    private final String descricao;           // Descrição do projeto
-    private final String dataInicio;          // Data de início do projeto
-    private final String dataTerminoPrevista; // Data de término prevista do projeto
-    private final String status;              // Status do projeto (aceitando os valores planejado, em andamento, concluído, cancelado)
-    private final Usuario gerenteResponsavel; // Nome do gerente responsável (objetos do tipo Usuario)
+// Importa as classes necessárias
+import java.io.Serializable;
 
-    // Construtor da classe
+class Projeto implements Serializable {
+    // Atributos principais da clase Projeto
+    private String nome;                // Nome do projeto
+    private String descricao;           // Descrição do projeto
+    private String dataInicio;          // Data de início do projeto
+    private String dataTerminoPrevista; // Data de término prevista do projeto
+    private String status;              // Status do projeto (aceitando os valores planejado, em andamento, concluído, cancelado)
+    private Usuario gerenteResponsavel; // Nome do gerente responsável (objeto do tipo Usuario)
+
+    // Construtor Padrão (Necessário para serialização)
+    public Projeto() { }
+
+    // Construtor principal para inicializar um projeto
     public Projeto(String nome, String descricao, String dataInicio, String dataTerminoPrevista, String status, Usuario gerenteResponsavel) {
         this.nome = nome;                               // Inicializa com o nome do projeto
         this.descricao = descricao;                     // Inicializa com a descrição do projeto
@@ -17,24 +23,22 @@ class Projeto {
         this.gerenteResponsavel = gerenteResponsavel;   // Inicializa com o gerente responsável pelo projeto
     }
 
-    //Getters, permitem acessar os atributos do projeto
+    // Métodos getters para acessar os dados
     public String getNome() { return nome; }
     public String getDescricao() { return descricao; }
     public String getDataInicio() { return dataInicio; }
     public String getDataTerminoPrevista() { return dataTerminoPrevista; }
     public String getStatus() { return status; }
-    public String getGerenteResponsavel() { return gerenteResponsavel.getNomeCompleto(); }
+    public String getGerenteResponsavel() { return gerenteResponsavel.getNome(); }
 
-    // Sobrescreve o método toString()
-    // É chamado automaticamente quando o objeto Projeto é impresso
+    // Sobrescreve o método toString() para mostrar os dados do projeto
     @Override
     public String toString() {
-        return "\n"
-                + "Nome do projeto: " + getNome() + "\n"
+        return "Nome: " + getNome() + "\n"
                 + "Descrição: " + getDescricao() + "\n"
                 + "Data de início: " + getDataInicio() + "\n"
                 + "Data de término prevista: " + getDataTerminoPrevista() + "\n"
                 + "Status: " + getStatus() + "\n"
-                + "Gerente responsável (nome completo): " + getGerenteResponsavel() + "\n";
+                + "Gerente responsável: " + getGerenteResponsavel();
     }
 }
