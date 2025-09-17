3ba48dae23cceff9dcf8ecd7fde86d0e86bc8475
trabalhoanhembi <trabalhoA3uam2025@gmail.com>
2025-09-17 16:28:24 -0300

    Update Tarefa.java
    Atualização

--- src/Tarefa.java
+++ src/Tarefa.java
@@ -1,58 +1,45 @@
 // Importa as classes necessárias
-import java.util.ArrayList;
-import java.util.List;
+import java.io.Serializable;
 
-class Tarefa {
+class Tarefa implements Serializable {
     // Atributos principais da clase Equipe
-    private final String nome;                  // Nome da tarefa
-    private final String descricao;             // Descrição da tarefa
-    private final String prazo;                 // Prazo da tarefa
-    private final String status;                // Status do projeto (aceitando os valores pendente, em andamento, concluída)
-    private final List<Usuario> responsavel;    // Lista de membros da equipe (objetos do tipo Usuario)
-    private final List<Projeto> projetos;       // Lista de projetos da equipe (objetos do tipo Projeto)
+    private String nome;            // Nome da tarefa
+    private String descricao;       // Descrição da tarefa
+    private String prazo;           // Prazo da tarefa
+    private String status;          // Status do projeto (aceitando os valores pendente, em andamento, concluída)
+    private Usuario responsavel;    // Responsável pela tarefa (objeto do tipo Usuario)
+    private Projeto projeto;        // Projeto associado a tarefa (objeto do tipo Projeto)
 
-    // Construtor da classe
-    public Tarefa(String nome, String descricao, Usuario responsavel, String prazo, String status, ArrayList<Projeto> projeto) {
-        this.nome = nome;                       // Inicializa com o nome da tarefa
-        this.descricao = descricao;             // Inicializa com a descrição da tarefa
-        this.prazo = prazo;                     // Inicializa o prazo da tarefa
-        this.status = status;                   // Inicializa com o status da tarefa
-        this.responsavel = new ArrayList<>();   // Cria uma lista vazia de responsavel/usuário
-        adicionarMembro(responsavel);           // Inicializa/adiciona o responsavel/usuário
-        this.projetos = new ArrayList<>();      // Cria uma lista vazia de projetos
-        alocarProjeto(projeto);                 // Inicializa/adiciona/aloca o projeto
+    // Construtor Padrão (Necessário para serialização)
+    public Tarefa() { }
+
+    // Construtor principal para inicializar uma tarefa
+    public Tarefa(String nome, String descricao, Usuario responsavel, String prazo, String status, Projeto projeto) {
+        this.nome = nome;               // Inicializa com o nome da tarefa
+        this.descricao = descricao;     // Inicializa com a descrição da tarefa
+        this.prazo = prazo;             // Inicializa o prazo da tarefa
+        this.status = status;           // Inicializa com o status da tarefa
+        this.responsavel = responsavel; // Cria uma lista vazia de responsavel/usuário
+        this.projeto = projeto;         // Cria uma lista vazia de projetos
     }
 
-    // Método para adicionar um membro/usuário a equipe
-    public void adicionarMembro(Usuario usuario) {
-        assert responsavel != null;
-        responsavel.add(usuario);
-    }
-
-    // Método para adicionar/alocar umprojeto a equipe
-    public void alocarProjeto(ArrayList<Projeto> projeto) {
-        assert projetos != null;
-        projetos.add(projeto.getFirst());
-    }
-
-    //Getters, permitem acessar os atributos da tarefa
+    // Métodos getters para acessar os dados
     public String getNome() { return nome; }
     public String getDescricao() { return descricao; }
     public String getPrazo() { return prazo; }
     public String getStatus() { return status; }
-    public String getResponsavel() { return responsavel.toString(); }
-    public String getProjetos() { return projetos.toString(); }
+    public String getResponsavel() { return responsavel.getNome(); }
+    public String getProjetos() { return projeto.getNome(); }
 
     // Sobrescreve o método toString()
     // É chamado automaticamente quando o objeto Tarefa é impresso
     @Override
     public String toString() {
-        return "\n"
-                + "Tarefa: " + getNome() + "\n"
+        return "Tarefa: " + getNome() + "\n"
                 + "Descrição: " + getDescricao() + "\n"
                 + "Prazo: " + getPrazo() + "\n"
                 + "Status: " + getStatus() + "\n"
                 + "Responsável: " + getResponsavel() + "\n"
-                + "Projetos: " + getProjetos() + "\n";
+                + "Projeto: " + getProjetos();
     }
 }
