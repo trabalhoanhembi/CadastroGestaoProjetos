66b116534cf0792b687ef5716c9673d51cc2ebec
trabalhoanhembi <trabalhoA3uam2025@gmail.com>
2025-09-19 20:16:33 -0300

    Update Tarefa.java
    Atualização

--- src/Tarefa.java
+++ src/Tarefa.java
@@ -1,8 +1,8 @@
 // Importa as classes necessárias
 import java.io.Serializable;
 
-class Tarefa implements Serializable {
-    // Atributos principais da clase Equipe
+public class Tarefa implements Serializable {
+    // Atributos principais da classe Equipe
     private String nome;            // Nome da tarefa
     private String descricao;       // Descrição da tarefa
     private String prazo;           // Prazo da tarefa
@@ -23,16 +23,33 @@ class Tarefa implements Serializable {
         this.projeto = projeto;         // Cria uma lista vazia de projetos
     }
 
-    // Métodos getters para acessar os dados
+    // Métodos setters
+    public void setNome(String nome) { this.nome = nome; }
+
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+
+    public void setPrazo(String prazo) { this.prazo = prazo; }
+
+    public void setStatus(String status) { this.status = status; }
+
+    public void setResponsavel(Usuario responsavel) { this.responsavel = responsavel; }
+
+    public void setProjeto(Projeto projeto) { this.projeto = projeto; }
+
+    // Métodos getters
     public String getNome() { return nome; }
+
     public String getDescricao() { return descricao; }
+
     public String getPrazo() { return prazo; }
+
     public String getStatus() { return status; }
+
     public String getResponsavel() { return responsavel.getNome(); }
+
     public String getProjetos() { return projeto.getNome(); }
 
-    // Sobrescreve o método toString()
-    // É chamado automaticamente quando o objeto Tarefa é impresso
+    // Sobrescreve o método toString() para mostrar os dados da tarefa
     @Override
     public String toString() {
         return "Tarefa: " + getNome() + "\n"
